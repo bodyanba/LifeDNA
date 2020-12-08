a019be42e489494bd2c26b0e00a89ea8627d5ab4
bodyanba <galkinbogdan@gmail.com>
2020-12-08 15:04:55 +0300

    input

--- js/common.js
+++ js/common.js
@@ -136,6 +136,14 @@ $(document).ready(function() {
 	
 	$('[data-toggle="popover"]').popover();
 
+	var input = document.querySelector("#phone");
+	var iti = window.intlTelInput(input, {
+	  // separateDialCode:true,
+	  utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@17.0.3/build/js/utils.js",
+	});
+	// store the instance variable so we can access it in the console e.g. window.iti.getNumber()
+	window.iti = iti;
+	
 	var typed = new Typed('.element', {
 		strings: ["metabolism", "energy", "cognition", "mental health", "natural detoxification process", "immune response", "brain health"],
 		typeSpeed: 50,
